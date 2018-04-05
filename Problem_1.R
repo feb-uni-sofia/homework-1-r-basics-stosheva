1b04850fb5cc8a9b0da94152e552ec6e8ecaa316
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-05 11:43:55 +0300

    Update Problem_1.R
    Paste solution from docx file.

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,25 @@
 
+x <- c(4, 1, 1, 4)
+x
+# b)
+y <- c(1, 4)
+y
+# c)
+x - y
+# result of the difference between x and y is 3 -3 0 0 
+# d)
+s <- c(x, y)
+# e)
+rep(s, 10)
+# resulting vector is called "es"
+es <- rep(s, 10)
+length(es)
+# f)
+rep(s, each =3)
+# g)
+seq(7, 21, by=1)
+7:21
+# h)
+# the sequence is called "h"
+h <- seq(7, 21, by=1)
+length(h)
