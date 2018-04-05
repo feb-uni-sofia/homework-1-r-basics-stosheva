b8d814982c65c4fd80c57967f1724f926bb4fc99
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-05 12:29:58 +0300

    Add review

--- Problem_1.R
+++ Problem_1.R
@@ -1,3 +1,6 @@
+## WARNING: please write your solution in the files Problem_1.R, Problem_2.R, etc. 
+## otherwise your files will _not_ be reviewed and will _not_ graded.
+
 # a)
 x <- c(4, 1, 1, 4)
 x
@@ -7,6 +10,8 @@ y
 # c)
 x - y
 # result of the difference between x and y is 3 -3 0 0 
+## True. But the question was why...
+
 # d)
 s <- c(x, y)
 # e)
--- Problem_2.R
+++ Problem_2.R
@@ -3,6 +3,8 @@
 xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
 xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
 date <- c('03Mon18', '04Tue18', '05Wed18', '06Thu18', '07Fri18', '08Sat18', '09Sun18')
+## Don't overwrite built-in objects like date.
+
 # b)
 xmax - xmin
 # c)
@@ -13,8 +15,15 @@ averagexmax
 # d)
 xmin[xmin < mean(xmin)]
 # e)
+## Not relevant
+
 xmin[xmin > mean(xmin)]
+
+## Correct is 
+xmin[xmax > mean(xmax)]
+
 # f) 
+## Nice
 names(xmin) <- date
 names(xmax) <- date
 names(xmax)
@@ -23,7 +32,9 @@ names(xmin)
 temperatures <- data.frame(xmin,xmax)
 temperatures
 # h)
-temperatures <- within(temperatures, {xminFahrenheit <- (xmin*9/5+32) })
+temperatures <- within(temperatures, {
+	xminFahrenheit <- (xmin*9/5+32) 
+})
 temperatures
 
 
